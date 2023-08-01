d25102a41a13a85ccfbc1a37b6d7812313a7dee7
Ignacio Iglesias <ignacioiglesias8@gmail.com>
2023-08-01 19:05:19 -0300

    trabajando sobre la ruta product

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,27 +1,51 @@
 import { Router} from 'express';
+import ProductManager from "../ProductManager.js";
 
 const router = Router();
 
-const users = [
-    {
-        name: "Nacho",
-        course: "Backend",
-    }
-];
+const productManager = new ProductManager('./products.json');
 
-router.get('/', (req,res)=>{
-    res.send(users)
+router.get('/', async (req, res) => {
+    const products = await productManager.getProducts();
+    const limit = parseInt(req.query.limit);
+    let productsToSend = products;
+    
+    if (!isNaN(limit)) {
+        productsToSend = products.slice(0, limit);
+    }
+
+    res.send(productsToSend)
+})
+
+router.get('/:pid', async (req, res) => {
+    const productId = parseInt(req.params.pid);
+    const product = await productManager.getProductById(productId);
+
+    if (!product) {
+        return res.send({
+            error: 'Producto no encontrado'
+        });
+    }
+
+    res.send({product});
 })
 
 router.post('/', (req,res)=> {
-    const user = {
-        name: req.body.name ?? 'Sin nombre',
-        course : req.body.course ?? 'Sin curso',
+    const product = {
+        id: req.body.id ?? 'Sin nombre',
+        title : req.body.title ?? 'Sin curso',
+        description: req.body.description ?? 'Sin descripción',
+        code: req.body.code ?? 'Sin código',
+        price: req.body.price ?? 'Sin precio',
+        status: true,
+        stock: req.body.stock ?? 'Sin stock',
+        category: req.body.category ?? 'Sin categoría',
+        thumbnails: req.body.thumbnails ?? 'Sin imagen'
     }
 
-    users.push(user);
+    users.push(product);
 
-    res.status(201).send ('Usuario creado correctamente')
+    res.status(201).send ('Proucto agregado correctamente')
 })
 
 export default router;
